c3b7513d4b28dc96a3e15cf72542f4bf86c352a9
ashok <ashokreddy.va180@gmail.com>
2019-09-24 20:26:50 +0530

    to check status

---
+++ CoreExamples/src/com/app/method/emaples/Admin.java
@@ -0,0 +1,22 @@
+package com.app.method.emaples;
+
+public class Admin {
+
+	static void message(boolean status)
+	{  
+		if(status)
+		{    
+			System.out.println("Yes! valid");   
+		}
+		else
+		{   
+			System.out.println("Sorry! Not valid");  
+		} 
+	}
+	public static void main(String[] args) {
+
+		Admin.message(true);
+		Admin.message(false);
+	}
+
+}
